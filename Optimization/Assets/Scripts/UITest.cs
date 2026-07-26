ecadaea24efaf4b2079f2b6b538c979e6fdf31c1
SadSmileXD <phycoeco@naver.com>
2026-07-26 16:07:32 +0900

    push

--- Optimization/Assets/Scripts/UITest.cs
+++ Optimization/Assets/Scripts/UITest.cs
@@ -1,16 +1,23 @@
-﻿using TMPro;
+﻿
+using TMPro;
 using UnityEngine;
-
+using System.Text;
 public class UITest : MonoBehaviour
 {
     public TextMeshProUGUI test;
-    
+    private StringBuilder sb = new StringBuilder(128);
+    public int score = 1000;
 
-    // Update is called once per frame
     void Update()
     {
-        test.text = "HP : " +
-            "Time : " + Time.deltaTime;
-
+        sb.Clear();
+        sb.Append("HP : ");
+        sb.Append("100");
+        sb.Append(" / ");
+        sb.Append("1000");
+        sb.Append(100);
+        sb.Append(1000);
+        sb.Append(10000);
+        test.text = sb.ToString(); // 오직 이 줄에서만 1회 할당!
     }
 }
