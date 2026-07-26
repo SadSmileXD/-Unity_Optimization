9568d7462a11140a65cc030255bdde1b39eec037
SadSmileXD <phycoeco@naver.com>
2026-07-26 13:02:13 +0900

    PUSH

---
+++ Optimization/Assets/Scripts/UITest.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class UITest : MonoBehaviour
+{
+    public TextMeshProUGUI test;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        test.text = "HP : " +
+            "Time : " + Time.deltaTime;
+
+    }
+}
